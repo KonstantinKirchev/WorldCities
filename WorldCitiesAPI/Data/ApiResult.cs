77e81087aae883e30842f3f038c377b6bac6dc63
Konstantin Kirchev <konstantin1230@yahoo.com>
2022-10-27 22:17:18 +0300

    Implementing filtering on the server side

--- WorldCitiesAPI/Data/ApiResult.cs
+++ WorldCitiesAPI/Data/ApiResult.cs
@@ -9,7 +9,7 @@
         /// <summary>
         /// Private constructor called by the CreateAsync method.
         /// </summary>
-        private ApiResult(List<T> data,int count,int pageIndex,int pageSize,string? sortColumn,string? sortOrder)
+        private ApiResult(List<T> data,int count,int pageIndex,int pageSize,string? sortColumn,string? sortOrder,string? filterColumn,string? filterQuery)
         {
             Data = data;
             PageIndex = pageIndex;
@@ -18,10 +18,12 @@
             TotalPages = (int)Math.Ceiling(count / (double)pageSize);
             SortColumn = sortColumn;
             SortOrder = sortOrder;
+            FilterColumn = filterColumn;
+            FilterQuery = filterQuery;
         }
         #region Methods
         /// <summary>
-        /// Pages and/or sorts a IQueryable source.
+        /// Pages, sorts and/or filters a IQueryable source.
         /// </summary>
         /// <param name="source">An IQueryable source of generic
         /// type</param>
@@ -32,12 +34,30 @@
         /// <param name="sortColumn">The sorting column name</param>
         /// <param name="sortOrder">The sorting order ("ASC" or
         /// "DESC")</param>
+        /// /// <param name="filterColumn">The filtering column
+        /// name</param>
+        /// <param name="filterQuery">The filtering query (value to
+        /// lookup)</param>
         /// <returns>
-        /// A object containing the paged result
-        /// /// and all the relevant paging navigation info.
+        /// A object containing the IQueryable paged/sorted/filtered
+        /// result
+        /// and all the relevant paging/sorting/filtering navigation
+        /// info.
         /// </returns>
-        public static async Task<ApiResult<T>> CreateAsync(IQueryable<T> source, int pageIndex, int pageSize, string? sortColumn = null, string? sortOrder = null)
+        public static async Task<ApiResult<T>> CreateAsync(
+            IQueryable<T> source, 
+            int pageIndex, 
+            int pageSize, 
+            string? sortColumn = null, 
+            string? sortOrder = null, 
+            string? filterColumn = null,
+            string? filterQuery = null)
         {
+            if (!string.IsNullOrEmpty(filterColumn) && !string.IsNullOrEmpty(filterQuery) && IsValidProperty(filterColumn))
+            {
+                source = source.Where(string.Format("{0}.StartsWith(@0)",filterColumn),filterQuery);
+            }
+
             var count = await source.CountAsync();
 
             if (!string.IsNullOrEmpty(sortColumn) && IsValidProperty(sortColumn))
@@ -52,7 +72,7 @@
 
             var data = await source.ToListAsync();
             
-            return new ApiResult<T>(data,count,pageIndex,pageSize,sortColumn,sortOrder);
+            return new ApiResult<T>(data,count,pageIndex,pageSize,sortColumn,sortOrder,filterColumn,filterQuery);
         }
 
         #region Methods
@@ -127,6 +147,15 @@
         /// Sorting Order ("ASC", "DESC" or null if none set)
         /// </summary>
         public string? SortOrder { get; set; }
+        /// <summary>
+        /// Filter Column name (or null if none set)
+        /// </summary>
+        public string? FilterColumn { get; set; }
+        /// <summary>
+        /// Filter Query string
+        /// (to be used within the given FilterColumn)
+        /// </summary>
+        public string? FilterQuery { get; set; }
         #endregion
     }
 }
