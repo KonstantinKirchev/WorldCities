fa8c4589e842ec3da0f99b0073baabf984871323
Konstantin Kirchev <konstantin1230@yahoo.com>
2022-10-28 16:32:30 +0300

    Fixing camel case issue

--- WorldCitiesAPI/Data/Models/Country.cs
+++ WorldCitiesAPI/Data/Models/Country.cs
@@ -3,6 +3,7 @@
     using Microsoft.EntityFrameworkCore;
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
+    using System.Text.Json.Serialization;
 
     [Table("Countries")]
     [Index(nameof(Name))]
@@ -24,10 +25,12 @@
         /// <summary>
         /// Country code (in ISO 3166-1 ALPHA-2 format)
         /// </summary>
+        [JsonPropertyName("iso2")]
         public string ISO2 { get; set; } = null!;
         /// <summary>
         /// Country code (in ISO 3166-1 ALPHA-3 format)
         /// </summary>
+        [JsonPropertyName("iso3")]
         public string ISO3 { get; set; } = null!;
         #endregion
         #region Navigation Properties
